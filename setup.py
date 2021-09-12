460a8e222e0dadc39c121af50c6dff5e92b4de52
Arya Aftab <arya.aftab@gmail.com>
2021-09-12 08:58:38 +0430

    Added Version 0.0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
+import os
+import codecs
 from setuptools import setup, find_packages
+
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(*parts):
+    with codecs.open(os.path.join(current_path, *parts), 'r', 'utf8') as reader:
+        return reader.read()
+        
+
 
 setup(
   name = 'sincnet-tensorflow',
   packages = find_packages(),
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'SincNet - Tensorflow',
+  long_description=read_file('README.md'),
+  long_description_content_type='text/markdown',
   author = 'Arya Aftab',
   author_email = 'arya.aftab@gmail.com',
   url = 'https://github.com/AryaAftab/sincnet-tensorflow',
